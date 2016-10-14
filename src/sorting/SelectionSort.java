d9612ad6758edd6573b22fcde6448c629b5ee36a
Abhishek Barla <abhishekbarla@Abhishek-Barla-Mac.local>
2016-10-13 22:27:39 -0700

    Implemented Selection Sort

--- src/sorting/SelectionSort.java
+++ src/sorting/SelectionSort.java
@@ -1,5 +1,6 @@
 package sorting;
 
+import common.CommandLineOutput;
 import repository.SortingRepository;
 
 /**
@@ -7,4 +8,50 @@ import repository.SortingRepository;
  * TODO
  */
 public class SelectionSort extends SortingRepository {
+
+
+	public static int[] doSelectionSort(int[] inputArray){
+
+		int temp = 0;
+		int minIndex = 0;
+		int[] array = new int[inputArray.length];
+
+		//Copying the input array to printout both the sorted and the unsorted array in the results
+		System.arraycopy(inputArray, 0, array, 0, inputArray.length);
+
+		//Selection Sort Logic
+		for(int i=0; i<array.length-1; i++)
+		{
+			//Set the i element as the min
+			minIndex = i;
+
+			//Finding the min value
+			for(int j=i+1; j<array.length; j++)
+			{
+				if(array[j]<array[minIndex])
+				{
+					minIndex = j;
+				}
+			}
+
+			//Swap
+			temp = array[i];
+			array[i] = array[minIndex];
+			array[minIndex] = temp;
+
+		}
+
+		return array;
+	}
+
+	public static void main(String[] args){
+
+		int[] unsortedArray = getRandomUnsortedArray();
+		long startTime = System.nanoTime();
+		int[] sortedArray = doSelectionSort(unsortedArray);
+		long endTime = System.nanoTime();
+		long sortingTime = endTime-startTime;
+
+		CommandLineOutput.sortingOutput("Selection Sort", unsortedArray, sortedArray, sortingTime);
+	}
 }
