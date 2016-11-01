085764e3f3f6ec98e5685b645b7a68685b42ce73
Abhishek Barla <barla.abhishek2010@gmail.com>
2016-10-31 22:13:58 -0700

    added merge sort implementation

--- src/sorting/MergeSort.java
+++ src/sorting/MergeSort.java
@@ -5,87 +5,85 @@ import util.CommonUtility;
 
 /**
  * Created by abhishekbarla on 10/10/16.
+ * Merge Sort implementation class
  *
  */
 public class MergeSort {
 
-	private static int[] unsortedArray;
-	private static int[] tempArray;
-
 	/**
-	 * Method to do MergeSort
-	 * @param lowerIndex
-	 * @param higherIndex
-	 */
-	public static void doMergeSort(int lowerIndex, int higherIndex){
-
-		int middleIndex;
-
-		//Ignoring when low>high as they are invalid indices
-		if(lowerIndex < higherIndex){
-
-			middleIndex = lowerIndex + (higherIndex-lowerIndex)/2;
-
-			//MergeSort on left half
-			doMergeSort(lowerIndex, middleIndex);
-			//MergeSort on right half
-			doMergeSort(middleIndex+1, higherIndex);
-
-			//Combine the two halves
-			mergeHalves(lowerIndex, middleIndex, higherIndex);
-		}
-	}
-
-	/**
-	 * Method to merge the halves
-	 * @param lowerIndex
-	 * @param middleIndex
-	 * @param higherIndex
-	 */
-	public static void mergeHalves(int lowerIndex, int middleIndex, int higherIndex){
-		//TODO
-
-		//Copy the original array into a temp array
-		for(int i=lowerIndex; i<middleIndex; i++){
-			tempArray[i] = unsortedArray[i];
-		}
-
-		int i = lowerIndex;
-		int j = middleIndex + 1;
-		int k = lowerIndex;
-
-		//Merge the array elements in increasing order
-		while(i<=middleIndex && j<=higherIndex){
-
-			if(tempArray[i] <= tempArray[j]){
-				unsortedArray[k] = tempArray[i];
-				i++;
-			}
-			else{
-				unsortedArray[k] = tempArray[j];
-				j++;
-			}
-			k++;
-		}
-
-		//If either of the two sub parts are empty
-		while(i<=middleIndex){
-			unsortedArray[k] = tempArray[i];
-			k++;
-			i++;
-		}
-	}
-
-
-	/**
-	 * Method to sort
+	 * Method to break the array down into smaller pieces using recursion
 	 * @param inputArray
 	 * @return
 	 */
-	public static int[] sort(int[] inputArray){
-		tempArray = new int[inputArray.length];
-		doMergeSort(0, inputArray.length-1);
-		return inputArray;
+	public static int[] doMergeSort(int[] inputArray){
+
+		int arrayLength = inputArray.length;
+		int[] left = new int[arrayLength/2];
+		int[] right = new int[arrayLength - arrayLength/2];
+		for(int i=0; i<left.length; i++){
+			left[i] = inputArray[i];
+		}
+		for(int j=0; j<right.length; j++){
+			right[j] = inputArray[j+arrayLength/2];
+		}
+
+		//Base Case
+		if(arrayLength<=1){
+			return inputArray;
+		}
+
+		int[] leftOutput = doMergeSort(left);
+		int[] rightOutput = doMergeSort(right);
+
+		return MergeParts(leftOutput, rightOutput);
+	}
+
+	/**
+	 * Method to merge the two parts of the array
+	 * @param left
+	 * @param right
+	 * @return
+	 */
+	public static int[] MergeParts(int[] left, int[] right){
+		int[] tempArray = new int[left.length + right.length];
+		int i = 0;
+		int j = 0;
+
+		for(int k=0; k<tempArray.length; k++){
+
+			//If left array has reached its end, simply add all remaining elements of the right array
+			if(i>=left.length){
+				tempArray[k] = right[j++];
+			}
+			//If right array has reached its end, simply add all remaining elements of the left array
+			else if(j>=right.length){
+				tempArray[k] = left[i++];
+			}
+			//If left array element is smaller, then add it to the final array
+			else if(left[i] <= right[j]){
+				tempArray[k] = left[i++];
+			}
+			//If the right array element is smaller, then add it to the final array
+			else {
+				tempArray[k] = right[j++];
+			}
+		}
+
+		return tempArray;
+	}
+
+	/**
+	 * Method to check if an array is sorted
+	 * @param inputArray
+	 * @return
+	 */
+	public static boolean isSorted(int[] inputArray){
+		for(int i=0; i<inputArray.length-1; i++){
+			if(inputArray[i] > inputArray[i+1]) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	/**
@@ -94,11 +92,12 @@ public class MergeSort {
 	 */
 	public static void main(String[] args){
 
-		unsortedArray = CommonUtility.randomArrayGenerator();
+		int[] unsortedArray = CommonUtility.randomArrayGenerator();
 		long startTime = System.nanoTime();
-		int[] sortedArray = sort(unsortedArray);
+		int[] sortedArray = doMergeSort(unsortedArray);
 		long endTime = System.nanoTime();
 		long sortingTime = endTime - startTime;
+		//System.out.println(isSorted(sortedArray));
 
 		CommandLineOutput.sortingOutput("Merge Sort", unsortedArray, sortedArray, sortingTime);
 	}
--- src/sorting/QuickSort.java
+++ src/sorting/QuickSort.java
@@ -2,6 +2,7 @@ package sorting;
 
 /**
  * Created by abhishekbarla on 10/11/16.
+ * //TODO
  */
 public class QuickSort {
 }
